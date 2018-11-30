9153e57cf61893fa1f1e982eb1a5ed9e54f7538f
Colin M Tindle <ampersand@underscore.space>
2018-11-30 11:42:08 +0000

    change: moved new Game validation to model where it should be

--- app/server/api/game/game-model.js
+++ app/server/api/game/game-model.js
@@ -1,17 +1,45 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const Player = require('../player/player-model')
 
 const team = {
   name: { type: String, required: false },
-  players: { type: [mongoose.Schema.Types.ObjectId], required: true },
+  players: {
+    type: [Schema.Types.ObjectId],
+    required: true,
+    validate: {
+      validator: async function(players) {
+        for (const player1 of players) {
+          const id = player1._id.toHexString()
+
+          if (!(await Player.findById(id))) {
+            // Player ID supplied is invalid
+            return false
+          }
+
+          // Only check for duplicates when in team1
+          if (players['_path'] === 'team1.players') {
+            // return false if Player ID appears in both teams
+            return !this.team2.players.find(player2 => player2._id.toHexString() === id)
+          }
+        }
+
+        return true
+      },
+      message: 'Invalid or Duplicate Player ID supplied'
+    }
+  },
   score: { type: Number, default: 0 }
 }
 
 module.exports = mongoose.model(
   'game',
-  new Schema({
-    date: { type: Date, default: Date.now },
-    team1: team,
-    team2: team
-  })
+  new Schema(
+    {
+      date: { type: Date, default: Date.now },
+      team1: team,
+      team2: team
+    },
+    { validateBeforeSave: true }
+  )
 )
